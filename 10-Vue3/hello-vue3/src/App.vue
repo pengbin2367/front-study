03b6a2b2bae2a02444428237df7816a34489043a
PengBin <pengbin2367@163.com>
2024-05-16 12:27:12 +0800

    feat: setup-vite-plugin-vue-setup-extend

--- 10-Vue3/hello-vue3/src/App.vue
+++ 10-Vue3/hello-vue3/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="app">
-    <Person/>
+    <Person />
   </div>
 </template>
 
@@ -8,16 +8,7 @@
 import Person from './components/Person.vue'
 
 export default {
-  name:'App', //组件名
-  components:{Person} //注册组件
+  name: 'App', //组件名
+  components: { Person } //注册组件
 }
 </script>
-
-<style>
-.app {
-  background-color: #ddd;
-  box-shadow: 0 0 10px;
-  border-radius: 10px;
-  padding: 20px;
-}
-</style>
--- 10-Vue3/hello-vue3/src/components/Person.vue
+++ 10-Vue3/hello-vue3/src/components/Person.vue
@@ -1,37 +1,25 @@
 <template>
   <div class="person">
-    <h2>姓名：{{name}}</h2>
-    <h2>年龄：{{age}}</h2>
+    <h2>姓名：{{ name }}</h2>
+    <h2>年龄：{{ age }}</h2>
     <button @click="changeName">修改名字</button>
     <button @click="changeAge">修改年龄</button>
     <button @click="showTel">查看联系方式</button>
   </div>
 </template>
 
-<script lang="ts">
-export default {
-  name:'Person',
-  data(){
-    return {
-      name:'张三',
-      age:18,
-      tel:'13888888888'
-    }
-  },
-  methods:{
-    // 修改姓名
-    changeName(){
-      this.name = 'zhang-san'
-    },
-    // 修改年龄
-    changeAge(){
-      this.age += 1
-    },
-    // 展示联系方式
-    showTel(){
-      alert(this.tel)
-    }
-  }
+<script setup lang="ts" name="Person234">
+let name = "zhang-san";
+let age = 23;
+let tel = 1234567890;
+function changeName() {
+  name = "zhang-san-update";
+}
+function changeAge() {
+  age++;
+}
+function showTel() {
+  alert(tel);
 }
 </script>
 
@@ -42,6 +30,7 @@ export default {
   border-radius: 10px;
   padding: 20px;
 }
+
 button {
   margin: 0 5px;
 }
