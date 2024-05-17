fd56664d13ebfe1cb9e02887f13cb501a830f1fb
PengBin <pengbin2367@163.com>
2024-05-17 21:34:33 +0800

    feat: slot

--- 10-Vue3/hello-vue3/src/router/index.ts
+++ 10-Vue3/hello-vue3/src/router/index.ts
@@ -1,27 +1,84 @@
-// 1. import createRouter
-import {createRouter, createWebHistory} from 'vue-router'
-import Person from "@/components/Person.vue";
-import Foo from "@/components/Foo.vue";
-import MyHook from "@/components/MyHook.vue";
+import { createRouter, createWebHistory } from 'vue-router'
+import Props from '@/pages/01_props/Father.vue'
+import Event from '@/pages/02_custom-event/Father.vue'
+import Bus from '@/pages/03_mitt/Father.vue'
+import Model from '@/pages/04_v-model/Father.vue'
+import AttrsListeners from '@/pages/05_$attrs/Father.vue'
+import RefChildrenParent from '@/pages/06_$refs-$parent/Father.vue'
+import ProvideInject from '@/pages/07_provide-inject/Father.vue'
+import Pinia from '@/pages/08_pinia/Father.vue'
+import Slot from '@/pages/09_slot/Father.vue'
 
-// 2. create router
-const router = createRouter({
+export default createRouter({
     history: createWebHistory(),
     routes: [
         {
-            path: '/home',
-            component: Person
+            path: '/props',
+            component: Props
         },
         {
-            name: 'About',
-            path: '/about',
-            component: Foo
+            path: '/event',
+            component: Event
         },
         {
-            path: '/contact',
-            component: MyHook
+            path: '/mitt',
+            component: Bus
+        },
+        {
+            path: '/model',
+            component: Model
+        },
+        {
+            path: '/attrs',
+            component: AttrsListeners
+        },
+        {
+            path: '/ref-parent',
+            component: RefChildrenParent
+        },
+        {
+            path: '/provide-inject',
+            component: ProvideInject
+        },
+        {
+            path: '/pinia',
+            component: Pinia
+        },
+        {
+            path: '/slot',
+            component: Slot
         },
     ]
 })
 
-export default router
+
+
+
+
+// // 1. import createRouter
+// import {createRouter, createWebHistory} from 'vue-router'
+// import Person from "@/components/Person.vue";
+// import Foo from "@/components/Foo.vue";
+// import MyHook from "@/components/MyHook.vue";
+//
+// // 2. create router
+// const router = createRouter({
+//     history: createWebHistory(),
+//     routes: [
+//         {
+//             path: '/home',
+//             component: Person
+//         },
+//         {
+//             name: 'About',
+//             path: '/about',
+//             component: Foo
+//         },
+//         {
+//             path: '/contact',
+//             component: MyHook
+//         },
+//     ]
+// })
+//
+// export default router
